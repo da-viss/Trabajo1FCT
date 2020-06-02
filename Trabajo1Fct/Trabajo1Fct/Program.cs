09e99bcb6cadeff9060726824baccaedd398b4fa
alumno.104416@ies-azarquiel.es <dragoneitor12>
2020-06-02 18:35:39 +0200

    rewrerwerwrwedsfdsfsdfsdfsdfsdfsdfd

---
+++ Trabajo1Fct/Trabajo1Fct/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabajo1Fct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
