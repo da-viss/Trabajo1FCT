29f2e90c1332d148a3da191ed4ee3530e9603ead
alumno.104416@ies-azarquiel.es <dragoneitor12>
2020-06-02 18:39:33 +0200

    Proyecto

--- Trabajo1Fct/Trabajo1Fct/Program.cs
+++ Trabajo1Fct/Trabajo1Fct/Program.cs
@@ -6,7 +6,7 @@ namespace Trabajo1Fct
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World im David!");
         }
     }
 }
